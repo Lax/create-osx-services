f0179a0babaa6b797def3966c1b57918abb42ab3
Liu Lantao <liulantao@gmail.com>
2018-10-08 14:21:25 +0800

    init
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+describe "Application 'create-osx-services'" do
+  before do
+    @app = NSApplication.sharedApplication
+  end
+
+  it "has one window" do
+    @app.windows.size.should == 1
+  end
+end
